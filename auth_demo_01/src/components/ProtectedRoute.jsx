638e78b6610cbf3de1ac0f633feed83aa252519a
Nichita <kilslertb@gmail.com>
2025-06-05 16:54:37 +0300

    deleted unused pages/components

--- auth_demo_01/src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-function ProtectedRoute({ isAuthenticated, children, redirectTo }) {
-    return isAuthenticated ? children : <Navigate to={redirectTo} />;
-}
-
-export default ProtectedRoute;
